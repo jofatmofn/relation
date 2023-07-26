f7b62b1e4a3b8292a73ea465965c2f6451c06717
Suresh Kumar Ramasamy <jofatmofn@users.noreply.github.com>
2023-07-26 15:49:23 +0530

    After OAUTH redirection: application root --> context/registrationId (Accordingly "Authorised redirect URIs" configuration in oauth server is to be modified)

--- src/main/java/org/sakuram/relation/spring/AppWebSecurityConfigurerAdapter.java
+++ src/main/java/org/sakuram/relation/spring/AppWebSecurityConfigurerAdapter.java
@@ -1,10 +1,18 @@
 package org.sakuram.relation.spring;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.sakuram.relation.util.AppException;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
@@ -28,7 +36,17 @@ public class AppWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapte
 		.logout(l -> l
 				.logoutSuccessUrl("/").permitAll()
 				.invalidateHttpSession(false))
-		.oauth2Login();
+		.oauth2Login()
+        .successHandler(new AuthenticationSuccessHandler() {
+        	 
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                    Authentication authentication) throws IOException, ServletException {
+            	response.sendRedirect(request.getContextPath() + request.getServletPath().substring(request.getServletPath().lastIndexOf("/")));
+            	// TODO: Handle default/no context scenario
+            }
+        });
+		
 	}
 
 }
